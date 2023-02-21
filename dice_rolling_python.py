5f97982cfa2007de4594c631d42aa0709e7cfceb
pravinDayananda <pdayananda16@gmail.com>
2023-02-21 13:33:45 -0500

    Add files via upload

---
+++ dice_rolling_python.py
@@ -0,0 +1,61 @@
+import random
+
+def  roll_dice():
+    dice_drawing = {
+    1: (
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘",
+    ),
+    2: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    3: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    4: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    5: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    6: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    }
+
+    roll = input("Roll the dice? (Yes/No)" )
+
+    while roll.lower() == "Yes".lower():
+        dice1 = random.randint(1,6)
+        dice2 = random.randint(1,6)
+
+        print("dice rolled: {} and {}".format(dice1, dice2))
+        print("\n".join(dice_drawing[dice1]))
+        print("\n".join(dice_drawing[dice2]))
+
+        roll = input("Roll Again ? (yes/no): ")
+
+roll_dice()
